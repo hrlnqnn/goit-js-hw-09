6db8175663dc15c4ea5d788b147605444d409d43
Anastasiia Sukhorukova <hrlnqnn@gmail.com>
2022-11-14 16:25:03 +0200

    added js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+buttonStop.disabled = true;
+let timerId = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStart.addEventListener('click', () => {
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+});
